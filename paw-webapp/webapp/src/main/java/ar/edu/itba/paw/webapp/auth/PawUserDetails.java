49dfe91d0544186b357e7e4a9f00794c4c87e55b
Ian <ianszejer@gmail.com>
2022-09-16 12:08:33 -0300

    Login finished

---
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/auth/PawUserDetails.java
@@ -0,0 +1,18 @@
+package ar.edu.itba.paw.webapp.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+//Extiende el User de spring security no el mio
+public class PawUserDetails extends User {
+    //Usamos este constructor, ya que no usamos las flags, etc
+    public PawUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public PawUserDetails(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    }
+}
---
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -0,0 +1,87 @@
+package ar.edu.itba.paw.webapp.config;
+
+
+import ar.edu.itba.paw.webapp.auth.PawUserDetailsService;
+import com.sun.org.glassfish.gmbal.ManagedObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("ar.edu.itba.paw.webapp.auth")
+public class WebAuthConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)	throws	Exception	{
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception	{
+        http.sessionManagement()
+                   // .invalidSessionUrl("/login/sign-in")
+                .and().authorizeRequests()
+                    .antMatchers("/login/sign-in","/login/sign-up").anonymous()
+                    //.antMatchers("/admin/**").hasRole("ADMIN")
+                    .antMatchers("/profile/*").authenticated()
+                    .antMatchers("/reviewForm/*/*").authenticated()
+                    .antMatchers("/**").permitAll()
+                .and().formLogin()
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .loginPage("/login/sign-in")
+                    .defaultSuccessUrl("/",false)
+                .and().rememberMe()
+                    .rememberMeParameter("rememberMe")
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
+                 //   .key(loadRememberMeKey())
+                .and().logout()
+                    .logoutSuccessUrl("/login/sign-out")
+                    .logoutSuccessUrl("/login/sign-in")
+                .and().exceptionHandling()
+                    .accessDeniedPage("/403")
+                .and().csrf().disable();
+    }
+
+
+    private String loadRememberMeKey(){
+        try (Reader reader = new InputStreamReader(
+                getClass().getClassLoader().getResourceAsStream("rememberme.key")
+        )){
+            return FileCopyUtils.copyToString(reader);
+        }catch(IOException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public void	configure(final WebSecurity web)throws	Exception{
+        web.ignoring()
+                .antMatchers("/css/**",	"/js/**",	"/img/**",	"/favicon.ico",	"/403");
+    }
+    }
