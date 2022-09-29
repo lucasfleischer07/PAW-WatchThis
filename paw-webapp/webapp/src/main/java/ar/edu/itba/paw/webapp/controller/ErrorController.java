bedec826d8c26b6954e148b15cac01e36394a872
Mateo Perez Rivera <matperez@itba.edu.ar>
2022-09-28 23:23:22 -0300

    Error mesagges i18n

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -13,49 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 
         @RequestMapping(value = "/error", method = RequestMethod.GET)
         public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
-
             ModelAndView errorPage = new ModelAndView("errorPage");
-            String errorTitle = "";
-            String errorBody ="";
-            int httpErrorCode = getErrorCode(httpRequest);
-
-            switch (httpErrorCode) {
-                case 400: {
-                    errorTitle = "Bad Request";
-                    errorBody ="Request could not be processed due to bad syntax.";
-                    break;
-                }
-                case 401: {
-                    errorTitle = "Unauthorized";
-                    errorBody ="This page is not publicly available.";
-
-                    break;
-                }
-                case 404: {
-                    errorTitle = "Page not found";
-                    errorBody ="Sorry we can't find your page. Let's try to get you back on track.";
-
-                    break;
-                }
-                case 405: {
-                    errorTitle = "Method not allowed";
-                    errorBody ="The action is not allowed for this url.";
-
-                    break;
-                }
-                case 500: {
-                    errorTitle = "Internal Server Error";
-                    errorBody ="There was a server error while processing your request.";
-
-                    break;
-                }
-                default:{
-                    errorTitle = "Error:"+httpErrorCode;
-                }
-            }
-            errorPage.addObject("errorCode", httpErrorCode);
-            errorPage.addObject("errorTitle", errorTitle);
-            errorPage.addObject("errorBody", errorBody);
+            errorPage.addObject("errorCode",getErrorCode(httpRequest));
             return errorPage;
         }
 
