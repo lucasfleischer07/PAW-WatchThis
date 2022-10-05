5f3fc7e1855d9562f045e9fd37b09dd6edf0f429
Ian <ianszejer@gmail.com>
2022-10-04 23:32:01 -0300

    Adding verification to forgot-password

---
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/validations/ExistingEmail.java
@@ -0,0 +1,23 @@
+package ar.edu.itba.paw.webapp.validations;
+
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EmailExistingValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExistingEmail {
+
+    //error message
+    public String message() default "The email is not registered";
+    //represents group of constraints
+    public Class<?>[] groups() default {};
+    //represents additional information about annotation
+    public Class<? extends Payload>[] payload() default {};
+
+}
