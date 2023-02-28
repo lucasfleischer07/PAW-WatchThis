8d6a5eb87905861805c264ace6a93de39e900da6
Lucas Fleischer <lfleischer@itba.edu.ar>
2023-02-28 20:26:33 -0300

    ReportsApi updated to new queryParamssFunction and errors check

--- paw-webapp/frontend/src/api/ReportsApi.js
+++ paw-webapp/frontend/src/api/ReportsApi.js
@@ -1,42 +1,84 @@
 import {APPLICATION_JSON_TYPE, paths} from "../paths";
+import {fetchWithQueryParamsApi} from "./FetchWithQueryParams";
 
 export class ReportsApi {
     constructor() {
         this.basePath = `${paths.BASE_URL_API}${paths.REPORTS}`
     }
 
-    async getReportsByType(type) {
-        const res = await fetch(`${this.basePath}/${type}`)
-        return await res.json()
+    async getReportsByType(type, pageNumber, pageSize) {
+        try {
+            const apiUrl = `${this.basePath}/${type}`
+            const params = {pageNumber: pageNumber, pageSize: pageSize}
+            const options = {}
+            const res = await fetchWithQueryParamsApi(apiUrl, params, options)
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
     }
 
     // TODO: VERIFICAR VIEN ESTE PORUQE NI IDEA
     async addReviewReport(reviewId, reviewReportReasons) {
-        return  await fetch(`${this.basePath}/review/${reviewId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': APPLICATION_JSON_TYPE,
-            },
-            // TODO: reviewReportReasons ya tiene que ser un objeto en formato JSON para mandarle con al info
-            body: JSON.stringify(reviewReportReasons)
-        })
+        try {
+            const res = await fetch(`${this.basePath}/review/${reviewId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': APPLICATION_JSON_TYPE,
+                },
+                // TODO: reviewReportReasons ya tiene que ser un objeto en formato JSON para mandarle con al info
+                body: JSON.stringify(reviewReportReasons)
+            })
+
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
     }
 
     // TODO: VERIFICAR VIEN ESTE PORUQE NI IDEA
     async addCommentReport(reviewId, commentReportReasons) {
-        return  await fetch(`${this.basePath}/comment/${reviewId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': APPLICATION_JSON_TYPE,
-            },
-            // TODO: commentReportReasons ya tiene que ser un objeto en formato JSON para mandarle con al info
-            body: JSON.stringify(commentReportReasons)
-        })
+        try {
+            const res = await fetch(`${this.basePath}/comment/${reviewId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': APPLICATION_JSON_TYPE,
+                },
+                // TODO: commentReportReasons ya tiene que ser un objeto en formato JSON para mandarle con al info
+                body: JSON.stringify(commentReportReasons)
+            })
+
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
     }
 
     async deleteReport(contentId, type) {
-        await fetch(`${this.basePath}/deleteReport/${type}/${contentId}`, {
-            method: 'DELETE'
-        })
+        try {
+            const res = await fetch(`${this.basePath}/deleteReport/${type}/${contentId}`, {
+                method: 'DELETE'
+            })
+
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
     }
 }
