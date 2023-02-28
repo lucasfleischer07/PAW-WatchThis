07da50fdbf0e0a9645834e91eb90310acdf65f1f
Lucas Fleischer <lfleischer@itba.edu.ar>
2023-02-28 20:21:59 -0300

    CommentApi and ContentApi updated to new queryParamssFunction and errors check

--- paw-webapp/frontend/src/api/CommentApi.js
+++ paw-webapp/frontend/src/api/CommentApi.js
@@ -1,30 +1,63 @@
 import {APPLICATION_JSON_TYPE, paths} from "../paths";
+import {fetchWithQueryParamsApi} from "./FetchWithQueryParams";
 
 export class CommentApi {
     constructor() {
         this.basePath = `${paths.BASE_URL_API}${paths.COMMENT}`
     }
 
-    async getReviewComments(reviewId) {
-        const res = await fetch(`${this.basePath}/${reviewId}`)
-        return await res.json()
+    async getReviewComments(reviewId, pageNumber, pageSize) {
+        try {
+            const apiUrl = `${this.basePath}/${reviewId}`
+            const params = {pageNumber: pageNumber, pageSize: pageSize}
+            const options = {}
+            const res = await fetchWithQueryParamsApi(apiUrl, params, options)
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (error) {
+            return {error: true}
+        }
+
     }
 
     async createComment(reviewId, commentDetails) {
-        // TODO: Verificar si esta bien el contentDetails.id o como seria esa parte
-        return  await fetch(`${this.basePath}/${reviewId}/add`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': APPLICATION_JSON_TYPE,
-            },
-            // TODO: contentDetails ya tiene que ser un objeto en formato JSON para mandarle con al info
-            body: JSON.stringify(commentDetails)
-        })
+        try {
+            const res = await fetch(`${this.basePath}/${reviewId}/add`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': APPLICATION_JSON_TYPE,
+                },
+                // TODO: contentDetails ya tiene que ser un objeto en formato JSON para mandarle con al info
+                body: JSON.stringify(commentDetails)
+            })
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
+
     }
 
     async commentReviewDelete(commentId) {
-        await fetch(`${this.basePath}/delete/${commentId}`, {
-            method: 'DELETE'
-        })
+        try {
+            const res = await fetch(`${this.basePath}/delete/${commentId}`, {
+                method: 'DELETE'
+            })
+            // TODO: Verificar este que onda, si deveulve algo o no
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
+
     }
 }
--- paw-webapp/frontend/src/api/ContentApi.js
+++ paw-webapp/frontend/src/api/ContentApi.js
@@ -1,4 +1,5 @@
 import {APPLICATION_JSON_TYPE, paths} from "../paths";
+import {fetchWithQueryParamsApi} from "./FetchWithQueryParams";
 
 export class ContentApi {
     constructor() {
@@ -11,73 +12,167 @@ export class ContentApi {
     }
 
     async getSpecificContent(contentId) {
-        const res = await fetch(`${this.basePath}/specificContent/${contentId}`)
-        return await res.json()
+        try {
+            const res = await fetch(`${this.basePath}/specificContent/${contentId}`)
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
+
     }
 
+
+
     async updateContent(contentDetails) {
-        // TODO: Verificar si esta bien el contentDetails.id o como seria esa parte
-        return  await fetch(`${this.basePath}/editInfo/${contentDetails.id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': APPLICATION_JSON_TYPE,
-            },
-            // TODO: contentDetails ya tiene que ser un objeto en formato JSON para mandarle con al info
-            body: JSON.stringify(contentDetails)
-        })
+        try {
+            // TODO: Verificar si esta bien el contentDetails.id o como seria esa parte
+            const res = await fetch(`${this.basePath}/editInfo/${contentDetails.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': APPLICATION_JSON_TYPE,
+                },
+                // TODO: contentDetails ya tiene que ser un objeto en formato JSON para mandarle con al info
+                body: JSON.stringify(contentDetails)
+            })
+
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
     }
 
     // TODO: Chequear bien este
     async getContentImage(contentId) {
-        const res = await fetch(`${this.basePath}/${contentId}/contentImage`)
-        return await res.json()
+        try {
+            const res = await fetch(`${this.basePath}/${contentId}/contentImage`)
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+
+        }
+
     }
 
     async updateContentImage(contentId, image) {
-        const formData = new FormData();
-        formData.append("image", image, image.name)
-        // TODO: Verificar si esta bien el contentDetails.id o como seria esa parte
-        return  await fetch(`${this.basePath}/${contentId}/contentImage`, {
-            method: 'PUT',
-            headers: {},
-            // TODO: contentDetails ya tiene que ser un objeto en formato JSON para mandarle con al info
-            body: formData
-        })
+        try {
+            const formData = new FormData();
+            formData.append("image", image, image.name)
+            // TODO: Verificar si esta bien el contentDetails.id o como seria esa parte
+            const res = await fetch(`${this.basePath}/${contentId}/contentImage`, {
+                method: 'PUT',
+                headers: {},
+                // TODO: contentDetails ya tiene que ser un objeto en formato JSON para mandarle con al info
+                body: formData
+            })
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+
+        }
     }
 
     async deleteContent(contentId) {
-        await fetch(`${this.basePath}/${contentId}/deleteContent`, {
-            method: 'DELETE'
-        })
+        try {
+            const res = await fetch(`${this.basePath}/${contentId}/deleteContent`, {
+                method: 'DELETE'
+            })
+            // TODO: Ver bien este
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
     }
 
     async createContent(contentDetails) {
-        // TODO: Verificar si esta bien el contentDetails.id o como seria esa parte
-        return  await fetch(`${this.basePath}/create`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': APPLICATION_JSON_TYPE,
-            },
-            // TODO: contentDetails ya tiene que ser un objeto en formato JSON para mandarle con al info
-            body: JSON.stringify(contentDetails)
-        })
+        try {
+            // TODO: Verificar si esta bien el contentDetails.id o como seria esa parte
+            const res = await fetch(`${this.basePath}/create`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': APPLICATION_JSON_TYPE,
+                },
+                // TODO: contentDetails ya tiene que ser un objeto en formato JSON para mandarle con al info
+                body: JSON.stringify(contentDetails)
+            })
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
+
+
+
     }
 
-    // TODO: Ver bien ste que seria con paginacion
-    // async getContentByType(contentType) {
-    //     const res = await fetch(`${this.basePath}/${contentType}`)
-    //     return await res.json()
-    // }
+
+    async getContentByType(contentType, pageNumber, pageSize) {
+        try {
+            const apiUrl = `${this.basePath}/${contentType}`
+            const params = {pageNumber: pageNumber, pageSize: pageSize}
+            const options = {}
+            const res = await fetchWithQueryParamsApi(apiUrl, params, options)
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
+    }
 
     // TODO: Ver bien ste que seria con paginacion y filtros
-    // async filterContentByType(contentType) {
-    //     const res = await fetch(`${this.basePath}/${contentType}/filters`)
-    //     return await res.json()
-    // }
+    // TODO: Filters seria un objeto que adentro tiene el genre, duration y sortBy
+    async filterContentByType(contentType, pageNumber, pageSize, filters) {
+        try {
+            const apiUrl = `${this.basePath}/${contentType}/filters`
+            const params = {pageNumber: pageNumber, pageSize: pageSize, ...filters}
+            const options = {}
+            const res = await fetchWithQueryParamsApi(apiUrl, params, options)
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
+    }
 
-    // TODO: Ver bien ste que seria con paginacion
     async getContentReviewers(contentId) {
-        const res = await fetch(`${this.basePath}/${contentId}/reviewers`)
-        return await res.json()
+        try {
+            const res = await fetch(`${this.basePath}/${contentId}/reviewers`)
+            if(res.status !== 204) {
+                return {error: false, data: await res.json()}
+            } else {
+                return {error: false, data: []}
+            }
+        } catch (e) {
+            return {error: true}
+        }
+
     }
 }
