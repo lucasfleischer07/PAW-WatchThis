59b3beb5e67261585a97b180df761fdb8e0ba119
Lucas Fleischer <lfleischer@itba.edu.ar>
2023-07-09 20:24:09 -0300

    Deployment changes

--- paw-webapp/frontend/src/App.js
+++ paw-webapp/frontend/src/App.js
@@ -23,7 +23,7 @@ import SignUp from "./views/SignUp";
 
 function App() {
   return (
-      <BrowserRouter basename={process.env.REACT_APP_CONTEXT}>
+      <BrowserRouter basename={"/paw-2022b-3/"}>
           <AuthProvider>
 
               <Routes>
