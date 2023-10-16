fcc8aef035b36099d347a6af1278f3a5f7a7a76c
IanSzejer <ianszejer@gmail.com>
2023-10-16 12:22:53 -0300

    removing pageNumber from everywhere

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/queryParams/GetReviewsParams.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/queryParams/GetReviewsParams.java
@@ -21,7 +21,7 @@ public class GetReviewsParams {
     public static PageWrapper<Review> getReviewsByParams(final Long userId,
                                                          final Long contentId,
                                                          final Long reportedById,
-                                                         final int pageNumber,
+                                                         final int page,
                                                          UserService us,
                                                          ContentService cs,
                                                          ReviewService rs) {
@@ -31,18 +31,18 @@ public class GetReviewsParams {
         PageWrapper<Review> reviewList = null;
         if(contentId != null) {
             Content content = cs.findById(contentId).orElseThrow(ContentNotFoundException::new);
-            reviewList = rs.getAllReviews(content, pageNumber, REVIEW_AMOUNT);
+            reviewList = rs.getAllReviews(content, page, REVIEW_AMOUNT);
         } else if(reportedById != null) {
             final User loggedUser = us.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName()).orElseThrow(UserNotFoundException::new);
             if(loggedUser.getId() == reportedById) {
 //                TODO: Setear aca lo de la lista de reviews reportadas (borrar la  lineas de abajo, esta puesta solo para que no tire error al correrlo)
-                reviewList = rs.getAllUserReviews(loggedUser, pageNumber, REVIEW_AMOUNT);
+                reviewList = rs.getAllUserReviews(loggedUser, page, REVIEW_AMOUNT);
             } else {
                 throw new ForbiddenException();
             }
         } else {
             final User user = us.findById(userId).orElseThrow(UserNotFoundException::new);
-            reviewList = rs.getAllUserReviews(user, pageNumber, REVIEW_AMOUNT);
+            reviewList = rs.getAllUserReviews(user, page, REVIEW_AMOUNT);
         }
         return reviewList;
 
